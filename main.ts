2154d07d17abcd33b2df1300571f2dc516c0af2a
cioffstix <joel.cioffi@gmail.com>
2024-04-23 16:07:16 +0000

    ANGRY SQURREL

--- main.ts
+++ main.ts
@@ -14,6 +14,10 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, l
     game.splash("///YOU DIED///", "///\"CRABBY\" WAS HIT BY LEAF///")
     game.over(false, effects.dissolve)
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile12`, function (sprite, location) {
+    game.splash("///YOU DIED///", "///\"CRABBY\" WAS KILLED BY A SQUIRREL WITH AN ASSUALT RIFLE///")
+    game.over(false, effects.dissolve)
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile9`, function (sprite, location) {
     game.splash("///YOU DIED///", "///\"CRABBY\" STEPPED ON STICK///")
     game.over(false, effects.slash)
@@ -26,8 +30,8 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile8`, function (sprite, l
     game.splash("///YOU DIED///", "///\"CRABBY\" STEPPED ON STICK///")
     game.over(false, effects.slash)
 })
-let MUSIC = 0
 let DIE = 0
+let MUSIC = 0
 let CRABBY = sprites.create(assets.image`STEVENDACRAB`, SpriteKind.Player)
 controller.moveSprite(CRABBY, 100, 100)
 tiles.setTilemap(tilemap`POISONGRASSLEVEL`)
@@ -35,12 +39,12 @@ scene.cameraFollowSprite(CRABBY)
 tiles.placeOnTile(CRABBY, tiles.getTileLocation(3, 15))
 game.splash("EMOTIONAL DAMAGE", "press a to start")
 game.onUpdateInterval(5000, function () {
+    MUSIC = randint(1, 5)
+})
+game.onUpdateInterval(7000, function () {
     DIE = randint(0, 10)
 })
-game.onUpdateInterval(5000, function () {
-    MUSIC = randint(1, 3)
-})
-game.onUpdateInterval(1000, function () {
+game.onUpdateInterval(2000, function () {
     if (DIE == 7) {
         game.splash("///YOU DIED///", "///\"CRABBY\" WAS HIT BY SUN///")
         game.over(false, effects.smiles)
@@ -51,6 +55,10 @@ game.onUpdateInterval(randint(5000, 50000), function () {
         music.playMelody("B C5 G A E F D C ", 130)
     } else if (MUSIC == 2) {
         music.playMelody("E B C5 A B G A F ", 130)
+    } else if (MUSIC == 3) {
+        music.playMelody("G G E A A D D E ", 120)
+    } else if (MUSIC == 4) {
+        music.playMelody("G G E A A D D E ", 120)
     } else {
         music.playMelody("A F D G C B F G ", 130)
     }
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -3,8 +3,6 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
@@ -19,16 +17,24 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile9 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile10 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "POISONGRASSLEVEL":
-            case "level1":return tiles.createTilemap(hex`100010000202020601010102020202010102020202020101010201020404010101010202020101010202010101020402040201020101070802020202010102020101010101020a090401010c01010101010201010102020101010102010102020102020201010202010402020101020201070801010b01010101010102020202010a09010102020102010102020101010102020201020202010102020101010102020202010102020101070801010102020202010101010201010a09010101020202050102010101010b040101010101010401020201020401010102020202020c0101020201010101040202020202010202020202040103010102020101010102020202`, img`
+            case "level1":return tiles.createTilemap(hex`10001000020202060101010202010201010202020202010101020102040401010101020202010101020101010102040204020c020101070802020202010102010101010101020a090e01010c0101010101020101010202010101010201010d020102020201010201010402020101010201070801010b01010101010102010202010a09010102020d0201010202010101010f02020101020201010f020101010102020202010102020101070801010c02020202010101010201010a090101010102010501020101010101040101010101010401020201010401010102020202010c0101020201010101040202020202010202020202040103010102020101010102020202`, img`
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -45,7 +51,7 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,sprites.castle.tileGrass2,myTiles.tile3,myTiles.tile4,sprites.dungeon.stairNorth,myTiles.tile5,myTiles.tile6,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11], TileScale.Sixteen);
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,sprites.castle.tileGrass2,myTiles.tile3,myTiles.tile4,sprites.dungeon.stairNorth,myTiles.tile5,myTiles.tile6,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14], TileScale.Sixteen);
         }
         return null;
     })
@@ -53,8 +59,6 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
-            case "myTile":
-            case "tile1":return tile1;
             case "myTile1":
             case "tile3":return tile3;
             case "myTile2":
@@ -69,12 +73,20 @@ namespace myTiles {
             case "tile8":return tile8;
             case "myTile7":
             case "tile9":return tile9;
-            case "myTile0":
-            case "tile2":return tile2;
             case "myTile8":
             case "tile10":return tile10;
             case "myTile9":
             case "tile11":return tile11;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile12":
+            case "tile14":return tile14;
         }
         return null;
     })
